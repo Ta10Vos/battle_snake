1fbf0e815316d142e6c227f7529458a3fa302e52
Ta10Vos <tim.vos@student.hu.nl>
2026-06-24 21:08:58 +0200

    Game -> Move

--- src/main/java/nl/hu/bep/webservices/MoveResource.java
+++ src/main/java/nl/hu/bep/webservices/MoveResource.java
@@ -1,15 +1,16 @@
 package nl.hu.bep.webservices;
 
+import nl.hu.bep.model.requests.MoveRequest;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
-@Path("/")
-public class GameResource {
-    @GET
+public class MoveResource {
+    @POST
     @Path("/move")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String move(MoveRequest request) {
-
+    public Response move(MoveRequest request) {
+        
     }
 }
