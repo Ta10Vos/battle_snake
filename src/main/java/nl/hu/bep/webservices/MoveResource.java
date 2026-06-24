4240b9f27270ece8463f78e4f6e315a3315fee85
Ta10Vos <tim.vos@student.hu.nl>
2026-06-24 21:27:08 +0200

    Updated move

--- src/main/java/nl/hu/bep/webservices/MoveResource.java
+++ src/main/java/nl/hu/bep/webservices/MoveResource.java
@@ -5,12 +5,12 @@ import nl.hu.bep.model.requests.MoveRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
+@Path("/move")
 public class MoveResource {
     @POST
-    @Path("/move")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response move(MoveRequest request) {
-        
+
     }
 }
