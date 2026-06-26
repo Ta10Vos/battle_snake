3c538c74e73c460bdd9f4c96b8a90f037db19778
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 12:49:58 +0200

    Added resource for getting snake customization/information

---
+++ src/main/java/nl/hu/bep/battlesnake/webservices/SnakeResource.java
@@ -0,0 +1,43 @@
+package nl.hu.bep.battlesnake.webservices;
+
+import nl.hu.bep.battlesnake.model.Customization;
+import nl.hu.bep.battlesnake.model.requests.CustomizationRequest;
+import nl.hu.bep.battlesnake.model.responses.CustomizationResponse;
+import nl.hu.bep.battlesnake.util.PersistenceManager;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+@Path("snake/")
+public class SnakeResource {
+    @GET
+    @Path("/game")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getCustomization() {
+        Customization customization = PersistenceManager.getCustomization();
+        return Response
+                .status(Response.Status.OK)
+                .entity(new CustomizationResponse(customization))
+                .build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response setCustomization(CustomizationRequest request) {
+        Customization customization = new Customization(request);
+        boolean result = PersistenceManager.saveCustomization(customization);
+
+        if (result) {
+            return Response.ok().build();
+        } else {
+            return Response
+                    .status(500)
+                    .entity(Map.of("message", "Something went wrong while saving customization."))
+                    .build();
+        }
+    }
+}
