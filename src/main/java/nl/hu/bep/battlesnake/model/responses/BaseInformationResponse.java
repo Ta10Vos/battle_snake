b13faaff1f201ca9521ed14a4940c0ba67178e28
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 11:12:48 +0200

    Added extends to BaseInformationResponse

--- src/main/java/nl/hu/bep/battlesnake/model/responses/BaseInformationResponse.java
+++ src/main/java/nl/hu/bep/battlesnake/model/responses/BaseInformationResponse.java
@@ -1,4 +1,7 @@
 package nl.hu.bep.battlesnake.model.responses;
 
-public class BaseInformationResponse {
+public class BaseInformationResponse extends CustomizationResponse {
+    public String color = "#00ffff";
+    public String head = "default";
+    public String tail = "default";
 }
