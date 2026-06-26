afab9cccfd1873a244ac5f53170486fcbe062ae5
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 17:05:03 +0200

    BaseGameRequest no longer uses superclass objects

--- src/main/java/nl/hu/bep/battlesnake/model/requests/BaseGameRequest.java
+++ src/main/java/nl/hu/bep/battlesnake/model/requests/BaseGameRequest.java
@@ -1,10 +1,12 @@
 package nl.hu.bep.battlesnake.model.requests;
 
+import nl.hu.bep.battlesnake.model.Battlesnake;
+import nl.hu.bep.battlesnake.model.Board;
 import nl.hu.bep.battlesnake.model.Game;
 
 public abstract class BaseGameRequest {
     public Game game;
     public int turn;
-    public Object board;
-    public Object you;
+    public Board board;
+    public Battlesnake you;
 }
