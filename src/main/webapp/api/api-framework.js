c7139c97dd2799cd1b135a72d84965a286c60b01
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 13:58:05 +0200

    GET request snake from interface works!

--- src/main/webapp/api/api-framework.js
+++ src/main/webapp/api/api-framework.js
@@ -1,25 +1,29 @@
 export default class ApiFramework {
-    static apiRoute = "restservices/";
-    
-    static async getRequest(endpoint, params = {}) {
-        const response = await fetch(apiRoute + endpoint, {
+    apiRoute = "restservices/";
+
+    constructor(apiRoute = null) {
+        this.apiRoute = apiRoute ?? this.apiRoute;
+    }
+
+    async getRequest(endpoint, params = {}) {
+        const response = await fetch(this.apiRoute + endpoint, {
             method: "GET",
             headers: {"content-type": "application/json"},
             ...params
         });
 
-        const returnedData = await response.json();
-
         if (!response.status.toString().startsWith("4") && !response.status.toString().startsWith("5")) {
-            return JSON.parse(returnedData);
+            const returnedData = await response.json();
+            return returnedData;
         } else {
-            alert("Error: " + response.message ?? "Something went wrong.");
+            const message = response.message ?? "Something went wrong";
+            alert("Error: " + message);
             return false;
         }
     }
 
-    static async putRequest(endpoint, params) {
-        const response = await fetch(apiRoute + endpoint, {
+    async putRequest(endpoint, params) {
+        const response = await fetch(this.apiRoute + endpoint, {
             method: "PUT",
             headers: {"content-type": "application/json"},
             ...params
@@ -28,7 +32,7 @@ export default class ApiFramework {
         const returnedData = await response.json();
 
         if (!response.status.toString().startsWith("4") && !response.status.toString().startsWith("5")) {
-            return JSON.parse(returnedData);
+            return returnedData;
         } else {
             alert("Error: " + response.message ?? "Something went wrong.");
             return false;
--- src/main/webapp/snake/snake-service.js
+++ src/main/webapp/snake/snake-service.js
@@ -1,18 +1,22 @@
+import ApiFramework from "../api/api-framework.js";
+
 export default class SnakeService {
+    apiFramework = new ApiFramework("../restservices/");
+
     async getSnake() {
         //TODO: haal deze data van de server
-        return Promise.resolve({
-            apiversion: "1",
-            author: "de dapper student",
-            color: "#ff0000",
-            head: "default",
-            tail: "default",
-            version: "0.1"
-        });
+        return await this.apiFramework.getRequest("snake/");
+        // return Promise.resolve({
+        //     apiversion: "1",
+        //     author: "de dapper student",
+        //     color: "#ff0000",
+        //     head: "default",
+        //     tail: "default",
+        //     version: "0.1"
+        // });
     }
 
     async updateSnake(updatedSnake) {
-        //TODO: update je slang aan de server-kant met de nieuwe gegevens
-        return Promise.resolve();
+        return await this.apiFramework.putRequest("snake/", {"body": updatedSnake});
     }
 }
