b1e72be78b5c080015ec742cc59b5cb10274b53f
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 17:03:22 +0200

    Added TileType

---
+++ src/main/java/nl/hu/bep/battlesnake/enums/TileType.java
@@ -0,0 +1,21 @@
+package nl.hu.bep.battlesnake.enums;
+
+public enum TileType {
+    // Map Tiles
+    EMPTY,
+    WALL,
+    FOOD,
+    HAZARD,
+    // Enemy snakes
+    ENEMY_HEAD,
+    ENEMY_BODY,
+    ENEMY_TAIL,
+    // Friendly snakes
+    FRIENDLY_HEAD,
+    FRIENDLY_BODY,
+    FRIENDLY_TAIL,
+    // You
+    YOU_HEAD,
+    YOU_BODY,
+    YOU_TAIL,
+}
