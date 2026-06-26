1df99a8125b4c72d6bf31d0e5f4ac4d94dbe0f30
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 12:06:44 +0200

    Added tryRead to FileManager

--- src/main/java/nl/hu/bep/battlesnake/util/FileManager.java
+++ src/main/java/nl/hu/bep/battlesnake/util/FileManager.java
@@ -1,19 +1,18 @@
 package nl.hu.bep.battlesnake.util;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public abstract class FileManager {
-    public static boolean tryWriteJson(String fileDirectory, String fileName, String jsonToSave) {
+    public static boolean tryWriteToFile(String fileDirectory, String fileName, String stringToSave) {
         File file = new File(fileDirectory + "/" + fileName + ".json");
 
         Path path = file.toPath();
         try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.CREATE)) {
-            bw.write(jsonToSave);
+            bw.write(stringToSave);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             return false;
@@ -22,7 +21,28 @@ public abstract class FileManager {
         return true;
     }
 
-    public static boolean tryGetJson(String fileDirectory, String fileName) {
+    /**
+     * Returns null if unable to find or read the specified file, otherwise returns
+     * the JSON String of this file.
+     * @param fileDirectory the directory where your file is in
+     * @param fileName the name of your file
+     * @return null | String
+     */
+    public static String tryReadFile(String fileDirectory, String fileName) {
+        Path path = Paths.get(fileDirectory + "/" + fileName);
+        File file = new File(path.toAbsolutePath().toString());
 
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            return null;
+        }
+
+        return sb.toString();
     }
 }
