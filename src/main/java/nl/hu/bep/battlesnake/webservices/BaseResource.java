a1ffc87e49a8d246f90f23636c1e97fc8b2492d7
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 10:55:10 +0200

    Upgraded BaseResource

--- src/main/java/nl/hu/bep/battlesnake/webservices/BaseResource.java
+++ src/main/java/nl/hu/bep/battlesnake/webservices/BaseResource.java
@@ -6,9 +6,18 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/game")
+@Path("/")
 public class BaseResource {
     @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getIndex() {
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/game")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response getCustomization() {
