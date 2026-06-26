481a75e259fdb5db5b76afb641a179f53978fb36
Ta10Vos <tim.vos@student.hu.nl>
2026-06-26 11:20:41 +0200

    Added CustomizationRequest & Fix Cust...Response

---
+++ src/main/java/nl/hu/bep/battlesnake/model/requests/CustomizationRequest.java
@@ -0,0 +1,7 @@
+package nl.hu.bep.battlesnake.model.requests;
+
+public class CustomizationRequest {
+    public String apiversion;
+    public String author;
+    public String version;
+}
--- src/main/java/nl/hu/bep/battlesnake/model/responses/BaseInformationResponse.java
+++ src/main/java/nl/hu/bep/battlesnake/model/responses/BaseInformationResponse.java
@@ -1,7 +1,7 @@
 package nl.hu.bep.battlesnake.model.responses;
 
 public class BaseInformationResponse extends CustomizationResponse {
-    public String color = "#00ffff";
-    public String head = "default";
-    public String tail = "default";
+    public String apiversion = "1";
+    public String author = "Ta10Vos";
+    public String version = "0.0.1-alpha";
 }
