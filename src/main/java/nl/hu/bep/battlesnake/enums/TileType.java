08f97c233c242710fbab089fd9b5a6e6756266c8
Ta10Vos <tim.vos@student.hu.nl>
2026-06-28 18:25:35 +0200

    Updated TileType
    No more comments and better point allocation

--- src/main/java/nl/hu/bep/battlesnake/enums/TileType.java
+++ src/main/java/nl/hu/bep/battlesnake/enums/TileType.java
@@ -2,34 +2,37 @@ package nl.hu.bep.battlesnake.enums;
 
 public enum TileType {
     // Map Tiles
-    NONE,// 0p
-    EMPTY,//5p
-    WALL,//100p
-    FOOD,//-20p
-    HAZARD,//15p
-    ENEMY_HEAD,//100p
-    ENEMY_BODY,//100p
-    ENEMY_TAIL,//10p
+    NONE,
+    EMPTY,
+    WALL,
+    FOOD,
+    HAZARD,
+    ENEMY_HEAD,
+    ENEMY_BODY,
+    ENEMY_TAIL,
     // Enemy snake neighboring tiles. They
     // Friendly snakes
-    FRIENDLY_HEAD,//{HAZARD}p
-    FRIENDLY_BODY,//{HAZARD}p
-    FRIENDLY_TAIL,//{HAZARD}p
+    FRIENDLY_HEAD,
+    FRIENDLY_BODY,
+    FRIENDLY_TAIL,
     // You
-    YOU_HEAD,//100p
-    YOU_BODY,//100p
-    YOU_TAIL,//4p
+    YOU_HEAD,
+    YOU_BODY,
+    YOU_TAIL,
     /* Split section to know where neighbors are.
     If a neighbor has a lower value than the neighbor it gets compared against,
     then the current neighbor has higher priority.
      */
     neighbor_tiles,// Split section
-    FOOD_NEIGHBOR,//-10p
-    ENEMY_HEAD_NEIGHBOR,//50p
-    ENEMY_BODY_NEIGHBOR,//10p
-    ENEMY_TAIL_NEIGHBOR,//0p
-    YOU_BODY_NEIGHBOR,//15p
-    YOU_TAIL_NEIGHBOR;//10p
+    FOOD_NEIGHBOR,
+    ENEMY_HEAD_NEIGHBOR,
+    ENEMY_BODY_NEIGHBOR,
+    ENEMY_TAIL_NEIGHBOR,
+    YOU_BODY_NEIGHBOR,
+    YOU_TAIL_NEIGHBOR,
+    FRIENDLY_HEAD_NEIGHBOR,
+    FRIENDLY_BODY_NEIGHBOR,
+    FRIENDLY_TAIL_NEIGHBOR;
 
     public static boolean isNeighbor(TileType type) {
         return type.compareTo(TileType.neighbor_tiles) > 0;
@@ -45,22 +48,26 @@ public enum TileType {
         return this.compareTo(type) < 0;
     }
 
+    /**
+     * The baseline cost of all tiles. Edit these values in specific PlayStyle modes.
+     * @param type The type of the Tile you're getting the cost from.
+     * @return The point cost of the TileType
+     */
     public static int getCost(TileType type) {
         return switch (type) {
-            case EMPTY -> 5;
-            case WALL, ENEMY_HEAD, ENEMY_BODY, YOU_HEAD, YOU_BODY -> 100;
-            case FOOD -> -10;
-            case HAZARD -> 15;
-            case ENEMY_TAIL -> 10;
-            case FRIENDLY_HEAD, FRIENDLY_BODY -> 15;//hazard score
-            case FRIENDLY_TAIL -> 15;//hazard score
-            case YOU_TAIL -> 20;
-            case FOOD_NEIGHBOR -> -10;
-            case ENEMY_HEAD_NEIGHBOR -> 50;
-            case ENEMY_BODY_NEIGHBOR -> 10;
-            case ENEMY_TAIL_NEIGHBOR -> 0;
-            case YOU_BODY_NEIGHBOR -> 15;
-            case YOU_TAIL_NEIGHBOR -> 10;
+            case EMPTY -> 3;
+            case WALL, ENEMY_HEAD, ENEMY_BODY, YOU_HEAD, YOU_BODY -> 1000;
+            case FOOD -> 1;
+            case HAZARD -> 5;
+            case ENEMY_TAIL, YOU_TAIL -> 2;
+            case FRIENDLY_HEAD, FRIENDLY_BODY -> 2;
+            case FRIENDLY_TAIL -> 4;
+            case FOOD_NEIGHBOR -> 2;
+            case ENEMY_HEAD_NEIGHBOR -> 10;
+            case ENEMY_BODY_NEIGHBOR -> 5;
+            case ENEMY_TAIL_NEIGHBOR -> 2;
+            case YOU_BODY_NEIGHBOR -> 2;
+            case YOU_TAIL_NEIGHBOR -> 2;
             default -> 0;
         };
     }
