31e373356c6bbebd2dceb70454eebb28a1891e1d
Ta10Vos <tim.vos@student.hu.nl>
2026-06-28 22:05:01 +0200

    Added cost + toString to Point

--- src/main/java/nl/hu/bep/battlesnake/evaluation/Point.java
+++ src/main/java/nl/hu/bep/battlesnake/evaluation/Point.java
@@ -4,11 +4,25 @@ import nl.hu.bep.battlesnake.model.Coordinate;
 
 public class Point extends Coordinate {
     public Point previous;
+    private Integer cost;
+
+    public Point(Coordinate coordinate) {
+        super(coordinate.x, coordinate.y);
+    }
 
     public Point(int x, int y, Point previous) {
         this.x = x;
         this.y = y;
         this.previous = previous;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        if (this.cost != null) return;
+        this.cost = cost;
     }
 
     @Override
@@ -19,5 +33,14 @@ public class Point extends Coordinate {
     @Override
     public Point getOffset(int offsetX, int offsetY) {
         return new Point(x + offsetX, y + offsetY, this);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "y=" + y +
+                ", x=" + x +
+                ", cost=" + cost +
+                '}';
     }
 }
