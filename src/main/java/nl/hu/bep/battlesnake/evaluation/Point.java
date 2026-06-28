68ede456949be1583bb1f209d80531fd4e5cfc60
Ta10Vos <tim.vos@student.hu.nl>
2026-06-28 21:06:06 +0200

    Added getOffset Override for Point

--- src/main/java/nl/hu/bep/battlesnake/evaluation/Point.java
+++ src/main/java/nl/hu/bep/battlesnake/evaluation/Point.java
@@ -2,13 +2,22 @@ package nl.hu.bep.battlesnake.evaluation;
 
 import nl.hu.bep.battlesnake.model.Coordinate;
 
-public class Point {
-    public Coordinate coordinate;
+public class Point extends Coordinate {
     public Point previous;
 
-    public Point(Coordinate coordinate) {
-        this.coordinate = coordinate;
+    public Point(int x, int y, Point previous) {
+        this.x = x;
+        this.y = y;
+        this.previous = previous;
     }
 
+    @Override
+    public Point getOffset(int offset) {
+        return getOffset(offset, offset);
+    }
 
+    @Override
+    public Point getOffset(int offsetX, int offsetY) {
+        return new Point(x + offsetX, y + offsetY, this);
+    }
 }
