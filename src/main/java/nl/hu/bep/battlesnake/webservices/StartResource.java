4f0cbb3109bb2c735e8c4095bb04f2592d41ef03
Ta10Vos <tim.vos@student.hu.nl>
2026-06-28 22:24:11 +0200

    Updated End & Start resource

--- src/main/java/nl/hu/bep/battlesnake/webservices/StartResource.java
+++ src/main/java/nl/hu/bep/battlesnake/webservices/StartResource.java
@@ -1,7 +1,10 @@
 package nl.hu.bep.battlesnake.webservices;
 
+import nl.hu.bep.battlesnake.evaluation.MoveResult;
+import nl.hu.bep.battlesnake.evaluation.ShortestPath;
 import nl.hu.bep.battlesnake.map.MapBuilder;
 import nl.hu.bep.battlesnake.model.requests.StartRequest;
+import nl.hu.bep.battlesnake.model.responses.MoveResponse;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -13,6 +16,14 @@ public class StartResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response startGame(StartRequest request) {
-        return Response.ok().build();
+        MapBuilder builder = new MapBuilder(request.board, request.you, true);
+
+        ShortestPath pathFinder = new ShortestPath(builder.getMap());
+        MoveResult result = pathFinder.run(request.you.getHead(), request.board.getFood().get(0));
+        String move = result.getMoveType().toString();
+        return Response
+                .ok()
+                .entity(new MoveResponse(move, ""))
+                .build();
     }
 }
