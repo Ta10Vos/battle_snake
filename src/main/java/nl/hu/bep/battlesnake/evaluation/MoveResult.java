9fb9b20e7ce4d53a223fbf053becc1556d091a17
Ta10Vos <tim.vos@student.hu.nl>
2026-06-28 22:04:49 +0200

    Added getters for MoveResult

--- src/main/java/nl/hu/bep/battlesnake/evaluation/MoveResult.java
+++ src/main/java/nl/hu/bep/battlesnake/evaluation/MoveResult.java
@@ -2,7 +2,30 @@ package nl.hu.bep.battlesnake.evaluation;
 
 import nl.hu.bep.battlesnake.enums.MoveType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoveResult {
-    private int pathCost;
+    private List<Point> path = new ArrayList<>();
+    private Integer pathCost = 0;
     private MoveType moveType;
+
+    public MoveResult() {}
+
+    public void addToPath(Point point) {
+        path.add(0, point);
+        pathCost += point.getCost();
+    }
+
+    public int getPathCost() {
+        return pathCost;
+    }
+
+    public List<Point> getPath() {
+        return path;
+    }
+
+    public MoveType getMoveType() {
+        return moveType;
+    }
 }
